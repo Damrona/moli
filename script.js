7a4c8852edb74e4128f5fa528de01b0d3c503eae
Damrona <105252762+Damrona@users.noreply.github.com>
2025-06-19 20:49:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
         document.getElementById('intro-animation').style.opacity = '0';
         setTimeout(function() {
             // Redirige después de la animación (ej: a "game.html")
-            window.location.href = "game.html"; // Cambia este enlace
+            window.location.href = "https://damrona.github.io/catalogo"; // Cambia este enlace
         }, 500); // Espera medio segundo antes de redirigir
     }, 5000); // Duración total de la animación (3 segundos)
 };
